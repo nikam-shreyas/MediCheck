499250d19e3ff91c02e9501dc56853697c80c40f
ZeronP <nik4m.5hreyas@gmail.com>
2020-10-31 14:02:54 +0530

    Validations added

--- popup.js
+++ popup.js
@@ -6,9 +6,26 @@ document
   .getElementById("updateBtn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    let featureList = ipc.send(
-      "update-feature",
-      document.getElementById("fetchFeature").value
-    );
-    electron.remote.getCurrentWindow().close();
+    let val = document.getElementById("fetchFeature").value;
+    if (document.getElementById("feature").value != "categorical") {
+      let min = Number(
+        document.getElementById("fetchFeature").getAttribute("min")
+      );
+      let max = Number(
+        document.getElementById("fetchFeature").getAttribute("max")
+      );
+      val = Number(val);
+      if (val > max || val < min) {
+        document.getElementById("updateBtn").classList.remove("btn-primary");
+        document.getElementById("updateBtn").classList.add("btn-danger");
+        document.getElementById("errMsg").innerHTML =
+          "Value must be between <b>" + min + "</b> and <b>" + max + "</b>";
+      } else {
+        let featureList = ipc.send("update-feature", val);
+        electron.remote.getCurrentWindow().close();
+      }
+    } else {
+      let featureList = ipc.send("update-feature", val);
+      electron.remote.getCurrentWindow().close();
+    }
   });
